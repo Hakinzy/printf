59f727529bd03bd8066390536c82264e0acbb44e
Hakinzy <adeyemiisaac046@gmail.com>
2022-10-17 08:28:33 -0700

    main

--- main.h
+++ main.h
@@ -1,18 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
 #include <stdarg.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
-
 #include <limits.h>
-
 #include <stdlib.h>
-
 
 
 #define OUTPUT_BUF_SIZE 1024
@@ -194,43 +187,4 @@ int print_octal(va_list ap, params_t *params);
 
 
 /* simple_printers.c module */
-
-int print_from_to(char *start, char *stop, char *except);
-
-int print_rev(va_list ap, params_t *params);
-
-int print_rot13(va_list ap, params_t *params);
-
-
-
-/* print_number.c module */
-
-int _isdigit(int c);
-
-int _strlen(char *s);
-
-int print_number(char *str, params_t *params);
-
-int print_number_right_shift(char *str, params_t *params);
-
-int print_number_left_shift(char *str, params_t *params);
-
-
-
-/* params.c module */
-
-void init_params(params_t *params, va_list ap);
-
-
-
-/* string_fields.c modoule */
-
-char *get_precision(char *p, params_t *params, va_list ap);
-
-
-
-/* _prinf.c module */
-
-int _printf(const char *format, ...);
-
 #endif
